3ab93a60f32fa0d695c330f0582b985dc05e5578
Deblox <122655329+Debl0xMain@users.noreply.github.com>
2026-06-02 11:59:13 +0200

    add jquery , back joueur

--- app/src/Controller/PlayersController.php
+++ app/src/Controller/PlayersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
 use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,8 +16,27 @@ class PlayersController extends AbstractController
     #[Route('/players', name: 'app_players')]
     public function index(Request $request, PlayerRepository $playerRepository): Response
     {
+        $role = $request->query->get('role');
+
+        if ($role && 'all' !== $role) {
+            $players = $playerRepository->findBy(
+                ['role' => $role],
+                ['pseudo' => 'ASC']
+            );
+        } else {
+            $players = $playerRepository->findBy([], ['pseudo' => 'ASC']);
+        }
+
         return $this->renderPage($request, 'players', 'Joueurs - Naxera', [
-            'players' => $playerRepository->findBy([], ['pseudo' => 'ASC']),
+            'players' => $players,
+        ]);
+    }
+
+    #[Route('/players/{id}/modal', name: 'app_players_modal', methods: ['GET'])]
+    public function modal(Player $player): Response
+    {
+        return $this->render('pages/players/_player_modal_content.html.twig', [
+            'player' => $player,
         ]);
     }
 }
